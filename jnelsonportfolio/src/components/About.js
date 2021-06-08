a8eb1c17c730ae8ae25e0430a49a92db34fff732
jnelson <jennifer.nelson242@gmail.com>
2021-06-08 14:32:57 -0500

    about component rendering on home page, resume download functional

---
+++ jnelsonportfolio/src/components/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import profilePic from "../images/jnelprofilepic.jpg";
+
+function About (){
+    return(
+    <main className="container content">
+      <div className="row">
+        <article className="col-sm-12 col-md-8 col-lg-8 window">
+          <h1 className="pageTitles">About Me</h1>
+          <hr></hr>
+          <img src={profilePic} className="img-fluid rounded-circle" alt="photo of Jennifer Nelson" id="jnel"></img>
+            <p>As a current full-stack web development student with a strong
+              background in healthcare revenue cycle leadership, it's safe to say
+              that I have a passion for designing systems that work well with
+              people. I hope to use my new skills to create dynamic, user-friendly, and efficient
+              applications in either the health care or education fields.</p>
+            <p>I live in Minnesota with my spouse, daughter, and puppy. I enjoy the
+              outdoors, <a id="dragonBoat"href="https://www.hornetwatersports.com/blogs/news/beginners-guide-to-dragon-boat-racing">dragonboat racing</a>, knitting, and curling up with a good
+              book. </p>
+             
+        </article>
+      </div>
+    </main>
+       
+        
+    );
+}
+
+export default About;
--- jnelsonportfolio/src/pages/Home.js
+++ jnelsonportfolio/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {Navbar, Nav, Container, NavDropdown} from "react-bootstrap";
 import{Link} from "react-router-dom";
+import About from "../components/About";
 import Footer from "../components/Footer";
+import resume from "../images/jennifer_nelson_full_stack_resume.pdf";
 import "../css/Home.css"
 
 
@@ -17,18 +19,13 @@ function Home(){
         <Nav className="ms-auto">
           <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
           <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href={resume} download="Jennifer Nelson Resume">Download Resume</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
   </Navbar>
   {/* about component */}
+  <About/>
   {/* skills icons component */}
   {/* component which has clickable images to portfolio and contact pages */}
 
