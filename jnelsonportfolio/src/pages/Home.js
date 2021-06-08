1c2d55a32add3aa42d057da461cc3fe2558c5ee2
jnelson <jennifer.nelson242@gmail.com>
2021-06-08 10:29:40 -0500

    navbar added to all pages, navlinks successfully routing to correct pages

--- jnelsonportfolio/src/pages/Home.js
+++ jnelsonportfolio/src/pages/Home.js
@@ -1,8 +1,31 @@
 import React from "react";
+import {Navbar, Nav, Container, NavDropdown} from "react-bootstrap";
+import{Link} from "react-router-dom";
 
 
 function Home(){
-   return <div>Hello Home World!</div>
+   return (
+       <>
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand as={Link} to="/">Jennifer Nelson</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+   </>)
 }
 
 export default Home;
