dbfee5639ba900880319cfb809ff549c736b4ce8
jnelson <jennifer.nelson242@gmail.com>
2021-06-19 13:26:48 -0500

    added icons folder to assets, created skills component

---
+++ jnelsonportfolio/src/components/skills/Skills.js
@@ -0,0 +1,6 @@
+import React from "react";
+import {Row,Col} from "react-bootstrap";
+import bootstrap from "../../assets/images/icons/bootstrap.png";
+import css from "../../assets/images/icons/css.png";
+import git from "../../assets/images/icons/git.png";
+import 
