d6d54a6752e5cd59575ec11b135bbaf4fc1a0e30
jnelson <jennifer.nelson242@gmail.com>
2021-06-24 14:02:30 -0500

    replaced contact form with contact sheet

--- jnelsonportfolio/src/pages/Contact.js
+++ jnelsonportfolio/src/pages/Contact.js
@@ -6,12 +6,32 @@ function Contact() {
   return (
     <>
       <Navheader />
-      <main className="container content">
+    {/* contact sheet; placeholder for contact form */}
+    <main className="container content">
         <section className="row">
           <div className="col-sm-12 col-md-8 col-lg-8 window">
             <h1>Contact</h1>
             <hr></hr>
-            <div className="mb-3">
+           <div className="mb-3">
+            <p><i className="fab fa-linkedin-in mx-3"></i><a href="https://www.linkedin.com/in/jennifer-nelson-aa3a2ba7/">https://www.linkedin.com/in/jennifer-nelson-aa3a2ba7/</a></p> 
+            <p><i className="fab fa-github mx-3"></i><a href="https://github.com/jnel-221">https://github.com/jnel-221</a></p> 
+            <p><i className="fas fa-envelope mx-3"></i>jennifer.nelson242@gmail.com</p> 
+            <p><i className="fas fa-phone-alt mx-3"></i>503-522-6905</p>
+            </div>
+            
+            
+          
+          </div>
+        </section> 
+      </main> 
+
+      {/* contact form; will replace contact sheet once it's working */}
+      {/* <main className="container content">
+        <section className="row">
+          <div className="col-sm-12 col-md-8 col-lg-8 window">
+            <h1>Contact</h1>
+            <hr></hr>
+           <div className="mb-3">
               <label className="form-label">
                 Name
               </label>
@@ -54,8 +74,8 @@ function Contact() {
               (503) 522-6905
             </p>
           </div>
-        </section>
-      </main>
+        </section> 
+      </main>  */}
       <Footer />
     </>
   );
