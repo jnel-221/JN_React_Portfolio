5c8551eac54908c9df29b502422cdfb1dd1e616a
jnelson <jennifer.nelson242@gmail.com>
2021-06-26 16:02:55 -0500

    troubleshooting pdf

--- jnelsonportfolio/src/components/skills/Skills.js
+++ jnelsonportfolio/src/components/skills/Skills.js
@@ -12,9 +12,10 @@ function Skills(){
         <h2 className="heading">Skills</h2>
         <hr></hr>
         <Row xs={2} md={6} lg={6} className="justify-content-md-center">
-            {skills.map(icon =>(
+            {skills.map((icon, index) =>(
                 <Col className="pt-3">
-                    <img src={icon.icon} alt={icon.alt} className="img img-thumbnail img-fluid icon"></img>
+                    <img key={index.toString()} src={icon.icon} alt={icon.alt} className="img img-thumbnail img-fluid icon"></img>
+                
                 </Col>
             ))}
         </Row>
